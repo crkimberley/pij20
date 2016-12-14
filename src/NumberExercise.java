d323f0a01cab7eecc53ad5288e59849d042cb9a8
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-12-14 00:41:16 +0000

    Java 8 Stream exercises 1 - 10 complete, although single and parallel stream versions of double multiplication seem to give the same result, not showing the differences the question suggested might be there

--- src/NumberExercise.java
+++ src/NumberExercise.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.Collectors;
 
 /**
@@ -13,6 +14,8 @@ public class NumberExercise {
     private void launch() {
         List<Integer> numbers = Arrays.asList(1,2,3,4,5);
         List<Integer> numbers2 = Arrays.asList(3,4);
+        List<Integer> numbers3 = Arrays.asList(31,23,19,57,41,68);
+        List<Double> numbers4 = Arrays.asList(237.517,356.248,4123.1907,876.739);
         System.out.println(numbers);
         System.out.println(numbers2);
 
@@ -41,5 +44,36 @@ public class NumberExercise {
                         .map(y -> new int[] {x,y}))
                 .peek(p -> System.out.println(p[0] + "," + p[1]))
                 .collect(Collectors.toList());
+
+        System.out.println("\n" + numbers3);
+        int sum = numbers3
+                .parallelStream()
+                .reduce(0, Integer::sum);
+        System.out.println("sum = " + sum);
+
+        System.out.println("\n" + numbers3);
+        sum = numbers3
+                .parallelStream()
+                .mapToInt(n -> n)
+                .sum();
+        System.out.println("sum = " + sum);
+
+        System.out.println("\n" + numbers3);
+        sum = numbers3
+                .parallelStream()
+                .collect(Collectors.summingInt(Integer::intValue));
+        System.out.println("sum = " + sum);
+
+        System.out.println("\n" + numbers4);
+        double product = numbers4
+                .stream()
+                .reduce((x,y) -> x * y)
+                .get();
+        System.out.println("product = " + product);
+        product = numbers4
+                .parallelStream()
+                .reduce((x,y) -> x * y)
+                .get();
+        System.out.println("product = " + product + " parallel");
     }
 }
